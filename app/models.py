ccc67a495e4d4b6d7f2432b16bfef749700409bb
inshelligent <46545553+inshelligent@users.noreply.github.com>
2021-09-29 10:26:33 +0800

    Added classes for Users, Tribal & Vote

--- app/models.py
+++ app/models.py
@@ -6,8 +6,27 @@ class Contestant(db.Model):
     name = db.Column(db.String(80))
     age = db.Column(db.Integer)
     occupation = db.Column(db.String(80))
-    description = db.Column(db.Text)
+    description = db.Column(db.String(500))
     is_eliminated = db.Column(db.Boolean)
+    tribals = db.relationship('Tribal', backref=db.backref('contestant', lazy=False))
 
-    def __repr__(self):
-        return '<Contestant %r>' % self.name
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    firstname = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    user_level = db.Column(db.String(20), nullable=False)
+    score = db.Column(db.Integer, default=0)
+
+class Tribal(db.Model):
+    tribal_date = db.Column(db.DateTime, primary_key=True, nullable=False)
+    voted_out_id = db.Column(db.Integer, db.ForeignKey('contestant.id'), nullable=False)
+
+class Vote(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
+    tribal_date = db.Column(db.DateTime, db.ForeignKey('tribal.tribal_date'), primary_key=True, nullable=False)
+    first_choice_id = db.Column(db.Integer, db.ForeignKey('contestant.id'), nullable=False)
+    second_choice_id = db.Column(db.Integer, db.ForeignKey('contestant.id'))
+    third_choice_id = db.Column(db.Integer, db.ForeignKey('contestant.id'))
