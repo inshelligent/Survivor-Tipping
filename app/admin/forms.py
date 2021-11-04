3e3f7622b3a000efa6bace3ab6203f4826d5c3c9
inshelligent <46545553+inshelligent@users.noreply.github.com>
2021-11-04 11:18:26 +0800

    Added validator to contestant age

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,13 +1,13 @@
 from datetime import date
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, BooleanField, SelectField, DateField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, NumberRange
 
 class AddContestantForm(FlaskForm):
     country = StringField('Country', default='Australia')
     season = IntegerField('Season', default=6)
     name = StringField('Contestant Name', validators=[InputRequired(), Length(min=1, max=80)])
-    age = IntegerField('Age')
+    age = IntegerField('Age', validators=[InputRequired(), NumberRange(min=18, max=99, message="Contestants must be over 18")])
     occupation = StringField('Occupation')
     description = StringField('Description')
     is_eliminated = BooleanField('Is eliminated?', default=False)
