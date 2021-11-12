fe3b5590f802b4b0de5170d06aac751794bb5a31
HGC-teacher <85601103+HGC-teacher@users.noreply.github.com>
2021-11-13 10:08:23 +1100

    Added the voting rulles

--- app/forms.py
+++ app/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import InputRequired
 
 class AddVoteForm(FlaskForm):
     tribal_id = SelectField('Tribal Date:', coerce=int, validators = [InputRequired()])
-    first_choice_id = SelectField('First vote: Contestant name:', coerce=int, validators = [InputRequired()])
-    second_choice_id = SelectField('Second vote: Contestant name:', coerce=int)
-    third_choice_id = SelectField('Third vote: Contestant name:', coerce=int)
+    first_choice_id = SelectField('First vote:', coerce=int, description='Your first choice to go home', validators = [InputRequired()])
+    second_choice_id = SelectField('Second vote:', coerce=int)
+    third_choice_id = SelectField('Third vote:', coerce=int)
     submit = SubmitField('Place Vote')
 
