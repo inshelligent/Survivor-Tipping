dc458cc3182e320a845638bbccbf9ae610420277
HGC-teacher <85601103+HGC-teacher@users.noreply.github.com>
2021-11-13 17:34:13 +1100

    Fixed issue with voting without First Vote

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, SelectField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, NumberRange
 
 class AddVoteForm(FlaskForm):
     tribal_id = SelectField('Tribal Date:', coerce=int, validators = [InputRequired()])
-    first_choice_id = SelectField('First vote:', coerce=int, description='Your first choice to go home', validators = [InputRequired()])
+    first_choice_id = SelectField('First vote:', coerce=int, description='Your first choice to go home', validators = [InputRequired(), NumberRange(min=1, message="You must select a contestant in First Vote Field")])
     second_choice_id = SelectField('Second vote:', coerce=int)
     third_choice_id = SelectField('Third vote:', coerce=int)
     submit = SubmitField('Place Vote')
